cac49e20a0b9e53a792494004ac1fee00756b236
Ajanaku-Olamiposi <olamiposajanaku53@gmail.com>
2025-07-04 16:26:22 +0100

    Done

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,20 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // 1. Create a new list to hold the multiples.
+        // 2. Use a for loop to iterate from 1 to the specified length.
+        // 3. For each iteration, calculate the multiple of the number by multiplying it with the current index.
+        // 4. Add the calculated multiple to the list.  
+        // 5. Convert the list to an array and return it because the function requires an array return type.
+        var multiples = new List<double>();
+
+        for (int i = 1; i <= length; i++)
+        {
+            double result = number * (i);
+            multiples.Add(result);
+        }
+
+        return multiples.ToArray(); // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +42,13 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Step 1. Store the required range in a list
+        // Step 2. Remove the required range from the end of the list
+        // Step 3. Insert the required range to the beginning of the list 
+
+        List<int> reqRange = data.GetRange(data.Count - amount, amount);
+        data.RemoveRange(data.Count - amount, amount);
+        data.InsertRange(0, reqRange);
     }
 }
